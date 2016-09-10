bad44045d5d899116a43bd9eb420f8f34a7f6f6a
Christina Zhu <cszhu@ucdavis.edu>
2016-09-10 18:47:19 -0400

    v2 of front end

--- web/js.js
+++ web/js.js
@@ -17,14 +17,60 @@ function initMap() {
     map: map,
     title: 'Uluru (Ayers Rock)'
   });
+
   marker.addListener('click', function() {
     infowindow.open(map, marker);
   });
+
+  // Enter address
+  document.getElementById('btn').onclick = function() {
+    var geocoder = new google.maps.Geocoder();
+
+    var search = document.getElementById('address').value;
+    console.log(search);
+    geocodeAddress(geocoder, map);
+    map.setZoom(16);
+  }
+
+  // Didn't get towed
+  document.getElementById('noTow').onclick = function() {
+    var center = map.getCenter();
+    var lat = center.lat();
+    var lng = center.lng();
+    console.log(lat + " " + lng);
+  }
+
+  // Got towed
+  document.getElementById('gotTowed').onclick = function() {
+    var center = map.getCenter();
+    var lat = center.lat();
+    var lng = center.lng();
+    console.log(lat + " " + lng);
+  }
 }
 
-window.onload= function() {
-  document.getElementById('btn').onclick = function() {
-      var search = document.getElementById('address').value;
-      console.log(search);
-  }
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      myLoc = results[0].geometry.location;
+      var ryan = resultsMap.getBounds();
+      var NELat = ryan.getNorthEast().lat();
+      var NELng = ryan.getNorthEast().lng();
+      var SWLat = ryan.getSouthWest().lat();
+      var SWLng = ryan.getSouthWest().lng();
+
+      console.log(NELat + " " + NELng);
+      console.log(SWLat + " " + SWLng);
+      console.log(myLoc.lat() + " " + myLoc.lng());
+
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 }
